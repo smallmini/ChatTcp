0f921f6d71f6ce903eac0bc16ce5b571390a2113
LiXiaYu <aasll@126.com>
2015-09-04 17:05:29 +0800

    The First Upload

---
+++ ChatTcp/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ChatTcp
+{
+    partial class frmChat
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessageBox = new System.Windows.Forms.Label();
+            this.txtMyMessage = new System.Windows.Forms.TextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.txtGoalIP = new System.Windows.Forms.TextBox();
+            this.lblGoalIP = new System.Windows.Forms.Label();
+            this.txtMyName = new System.Windows.Forms.TextBox();
+            this.lblMyName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessageBox
+            // 
+            this.lblMessageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMessageBox.Location = new System.Drawing.Point(8, 7);
+            this.lblMessageBox.Name = "lblMessageBox";
+            this.lblMessageBox.Size = new System.Drawing.Size(288, 250);
+            this.lblMessageBox.TabIndex = 0;
+            this.lblMessageBox.Text = "HELLO,EYERYONE!\r\n";
+            // 
+            // txtMyMessage
+            // 
+            this.txtMyMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMyMessage.Location = new System.Drawing.Point(10, 260);
+            this.txtMyMessage.Multiline = true;
+            this.txtMyMessage.Name = "txtMyMessage";
+            this.txtMyMessage.Size = new System.Drawing.Size(248, 38);
+            this.txtMyMessage.TabIndex = 1;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSendMessage.Location = new System.Drawing.Point(263, 260);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(70, 37);
+            this.btnSendMessage.TabIndex = 2;
+            this.btnSendMessage.Text = "发送";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // txtGoalIP
+            // 
+            this.txtGoalIP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtGoalIP.Location = new System.Drawing.Point(189, 309);
+            this.txtGoalIP.Name = "txtGoalIP";
+            this.txtGoalIP.Size = new System.Drawing.Size(144, 21);
+            this.txtGoalIP.TabIndex = 3;
+            this.txtGoalIP.TextChanged += new System.EventHandler(this.txtGoalIP_TextChanged);
+            // 
+            // lblGoalIP
+            // 
+            this.lblGoalIP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGoalIP.AutoSize = true;
+            this.lblGoalIP.Location = new System.Drawing.Point(130, 312);
+            this.lblGoalIP.Name = "lblGoalIP";
+            this.lblGoalIP.Size = new System.Drawing.Size(53, 12);
+            this.lblGoalIP.TabIndex = 4;
+            this.lblGoalIP.Text = "目标ip：";
+            // 
+            // txtMyName
+            // 
+            this.txtMyName.Location = new System.Drawing.Point(65, 309);
+            this.txtMyName.Name = "txtMyName";
+            this.txtMyName.Size = new System.Drawing.Size(59, 21);
+            this.txtMyName.TabIndex = 5;
+            // 
+            // lblMyName
+            // 
+            this.lblMyName.AutoSize = true;
+            this.lblMyName.Location = new System.Drawing.Point(12, 312);
+            this.lblMyName.Name = "lblMyName";
+            this.lblMyName.Size = new System.Drawing.Size(41, 12);
+            this.lblMyName.TabIndex = 6;
+            this.lblMyName.Text = "名字：";
+            // 
+            // frmChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 342);
+            this.Controls.Add(this.lblMyName);
+            this.Controls.Add(this.txtMyName);
+            this.Controls.Add(this.lblGoalIP);
+            this.Controls.Add(this.txtGoalIP);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtMyMessage);
+            this.Controls.Add(this.lblMessageBox);
+            this.Name = "frmChat";
+            this.Text = "Chat";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmChat_FormClosed);
+            this.Load += new System.EventHandler(this.frmChat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessageBox;
+        private System.Windows.Forms.TextBox txtMyMessage;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.TextBox txtGoalIP;
+        private System.Windows.Forms.Label lblGoalIP;
+        private System.Windows.Forms.TextBox txtMyName;
+        private System.Windows.Forms.Label lblMyName;
+    }
+}
+
---
+++ ChatTcp/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatTcp
+{
+    public partial class frmChat : Form
+    {
+        /// <summary>
+        /// 目标ip
+        /// </summary>
+        string goalIP= "192.168.1.130";
+        /// <summary>
+        /// 本地ip
+        /// </summary>
+        IPAddress myIP;
+        /// <summary>
+        /// 服务器监听线程
+        /// </summary>
+        Thread listenerThread;
+        
+
+        public frmChat()
+        {
+            InitializeComponent();
+        }
+
+        private void frmChat_Load(object sender, EventArgs e)
+        {
+            //获取ip地址
+            this.GetAddressIP();
+            this.txtGoalIP.Text = this.myIP.ToString();
+
+            //从新线程开始服务器监听
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            this.listenerThread = new Thread(new ThreadStart(this.ListenerRead));
+            listenerThread.Start();
+        }
+        
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            
+                this.ClientWrite();
+        }
+        
+        /// <summary>
+        /// 服务器端接收客户端消息
+        /// </summary>
+        private void ListenerRead()
+        {
+            while(true)
+            {
+                try
+                {
+                    TcpListener tcpListener = new TcpListener(this.myIP,2015);
+                    tcpListener.Start();
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    NetworkStream netStream = tcpClient.GetStream();
+
+                    byte[] buf = new byte[1000];
+
+                    netStream.Read(buf, 0, 1000);
+
+                    this.lblMessageBox.Text += Encoding.Default.GetString(buf);
+
+                    netStream.Close();
+                    tcpClient.Close();
+                    tcpListener.Stop();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            
+            }
+                
+        }
+
+        /// <summary>
+        /// 客户端向服务器端发送消息
+        /// </summary>
+        private void ClientWrite()
+        {
+            
+            try
+            {
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(this.goalIP, 2015);
+                NetworkStream netStream = tcpClient.GetStream();
+
+                byte[] buf = Encoding.Default.GetBytes("\x000A"+DateTime.Now.ToString()+"  "+this.txtMyName.Text+":\x000A"+this.txtMyMessage.Text);
+
+                netStream.Write(buf, 0, buf.Length);
+
+                netStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// 获取本地IP地址信息
+        /// </summary>
+        void GetAddressIP()
+        {
+            foreach (IPAddress _IPAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (_IPAddress.AddressFamily.ToString() == "InterNetwork")
+                {
+                    this.myIP = _IPAddress;
+                    break;
+                }
+            }
+            this.Text += this.myIP.ToString();
+        }
+
+        private void txtGoalIP_TextChanged(object sender, EventArgs e)
+        {
+            //获取目标ip
+            this.goalIP = this.txtGoalIP.Text;
+        }
+
+        private void frmChat_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //强制退出所有线程
+            System.Environment.Exit(0);
+        }
+    }
+}
